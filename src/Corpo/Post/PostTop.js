dabcdee0a5d99623964c88eacabd8d131d8de32d
instagramReact <instagramreactprojeto@gmail.com>
2024-12-02 20:37:25 -0300

    instagramReact

--- src/Corpo/Post/PostTop.js
+++ src/Corpo/Post/PostTop.js
@@ -1,25 +1,44 @@
+import React from "react";
+
 export default function PostTop(props) {
+ 
+  const handleDoubleClick = (event) => {
+  
+    if (!props.heart && event.detail === 2) {
+      props.setHeart(true); 
+    } else if (props.heart && event.detail === 2) {
+      props.setHeart(false); 
+    }
+  };
+
   return (
     <div>
-      <div class="topo">
-        <div class="usuario">
-          <img src={props.nickImg} />
+      <div className="topo">
+        <div className="usuario">
+          <img src={props.nickImg} alt="Imagem do usuário" />
           {props.nick}
         </div>
-        <div class="acoes">
+        <div className="acoes">
           <ion-icon name="ellipsis-horizontal"></ion-icon>
         </div>
       </div>
 
-      <div class="conteudo">
+      <div className="conteudo">
         <img
-          onClick={(event) => {
-            if (!props.heart && event.detail === 2)
-              props.setHeart(!props.heart);
-          }}
           src={props.img}
+          alt="Imagem do post"
+          onDoubleClick={handleDoubleClick} 
         />
+        
+        <div className="coracao">
+          <ion-icon
+            name={props.heart ? "heart" : "heart-outline"} 
+            style={{ position: "absolute", top: "10px", left: "10px", fontSize: "30px", color: props.heart ? "red" : "black" }}
+          />
+        </div>
       </div>
     </div>
   );
 }
+
+
